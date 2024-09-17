c234f6eebc35e29d9783bc5a37913a18c142507a
Ryan <r1d8o0o9@gmail.com>
2024-09-17 09:06:17 -0400

    refactor to use setup function for test file

--- tests/Feature/ProjectCreationTest.php
+++ tests/Feature/ProjectCreationTest.php
@@ -8,63 +8,74 @@ use Tests\TestCase;
 
 class ProjectCreationTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->validProject = Project::factory()->raw();
+        $this->managerUser = User::factory()->manager()->create();
+        $this->regularUser = User::factory()->regular()->create();
+        $this->followingRedirects();
+    }
 
     public function test_a_regular_user_cannot_create_a_project(): void
     {
-        $validProject = Project::factory()->raw();
-        $regularUser = User::factory([
-            'is_manager' => 0
-        ])->regular()->create();
-
-        $response = $this->actingAs($regularUser)
+        $response = $this->actingAs($this->regularUser)
             ->fromRoute('projects.create')
-            ->post(route('projects.store'), $validProject);
+            ->post(route('projects.store'), $this->validProject);
 
         $response->assertForbidden();
-        $this->assertDatabaseMissing('projects', $validProject);
+        $this->assertDatabaseMissing('projects', $this->validProject);
     }
 
     public function test_a_manager_can_create_a_project(): void
     {
-        $validProject = Project::factory()->raw();
-        $managerUser = User::factory()->manager()->create();
-
-        $this->actingAs($managerUser)
+        $this->actingAs($this->managerUser)
             ->fromRoute('projects.create')
-            ->post(route('projects.store'), $validProject)
-            ->assertRedirect();
-        $this->assertDatabaseHas('projects', $validProject);
+            ->post(route('projects.store'), $this->validProject)
+            ->assertSee($this->validProject['title'])
+            ->assertSuccessful();
+        $this->assertDatabaseHas('projects', $this->validProject);
     }
 
     public function test_a_fresh_project_is_seen_on_dashboard(): void
     {
-        $validProject = Project::factory()->raw();
-        $managerUser = User::factory()->manager()->create();
-        $this->followingRedirects();
+        $this->validProject = Project::factory()->raw();
+        $this->managerUser = User::factory()->manager()->create();
 
-        $this->actingAs($managerUser)
+        $this->actingAs($this->managerUser)
             ->fromRoute('dashboard')
-            ->post(route('projects.store'), $validProject)
+            ->post(route('projects.store'), $this->validProject)
             ->assertSuccessful()
-            ->assertSee($validProject['title']);
-        $this->assertDatabaseHas('projects', $validProject);
+            ->assertSee($this->validProject['title']);
+        $this->assertDatabaseHas('projects', $this->validProject);
     }
 
     public function test_a_fresh_project_only_has_manager_as_member(): void
     {
-        $validProject = Project::factory()->raw();
-        $managerUser = User::factory()->manager()->create();
-        $this->followingRedirects();
-
-        $this->actingAs($managerUser)
+        $this->actingAs($this->managerUser)
             ->fromRoute('dashboard')
-            ->post(route('projects.store'), $validProject)
+            ->post(route('projects.store'), $this->validProject)
             ->assertSuccessful()
-            ->assertSee($validProject['title']);
-        $this->assertDatabaseHas('projects', $validProject);
-        $newProject = Project::query()->where('title', $validProject['title'])->first();
+            ->assertSee($this->validProject['title']);
+        $this->assertDatabaseHas('projects', $this->validProject);
+        $newProject = Project::query()->where('title', $this->validProject['title'])->first();
 
         $this->assertCount(1, $newProject->assignees);
-        $this->assertTrue($newProject->assignees->first()->is($managerUser));
+        $this->assertTrue($newProject->assignees->first()->is($this->managerUser));
+    }
+
+    public function test_manager_can_add_regular_assignees_to_new_project(): void
+    {
+        $this->markTestSkipped();
+
+        $this->actingAs($this->managerUser)
+            ->fromRoute('dashboard')
+            ->post(route('projects.store'), $this->validProject)
+            ->assertSuccessful();
+        $this->assertDatabaseHas('projects', $this->validProject);
+
+//        $newProject = Project::query()->where('title', $this->validProject['title'])->first();
+//        $this->assertCount(2, $newProject->assignees);
+//        $this->assertTrue($newProject->assignees->second()->is($regularUser));
     }
 }
